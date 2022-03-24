8f70c3804fdeb39d8d35f96f6297f9d1269cfb23
Zhijun Li <zli2@student.sierracollege.edu>
2022-03-24 15:31:55 -0700

    Updated main.sh

--- main.sh
+++ main.sh
@@ -6,9 +6,8 @@ deleteFile()
     	rm "$FILE"
 	fi
 }
-getGeoLocation()
+getWeatherData()
 {
-	deleteFile location.txt
 	latitude=$(curl -s https://json.geoiplookup.io/"$(curl -s https://ipinfo.io/ip)" | jq '.latitude')
 	longitude=$(curl -s https://json.geoiplookup.io/"$(curl -s https://ipinfo.io/ip)" | jq '.longitude')
 	echo "$latitude"
@@ -18,5 +17,8 @@ getGeoLocation()
     chmod 600 weatherData.json
 }
 
-getGeoLocation 
+getWeatherData 
+bash createTable.sh
 python3 main.py
+
+
