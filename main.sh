5966dbd300e4c35e5514dcf4a8122f6923cbe879
Zhijun Li <zli2@student.sierracollege.edu>
2022-03-25 23:52:55 -0700

    Updated main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 deleteFile()
 {
 	FILE=$1
@@ -8,17 +8,22 @@ deleteFile()
 }
 getWeatherData()
 {
+	#初步想改成这样
+	curl http://ip-api.com/json/ | jq . > ip.json
+	#接着用awk抓latitude和longitude
+	
+
 	latitude=$(curl -s https://json.geoiplookup.io/"$(curl -s https://ipinfo.io/ip)" | jq '.latitude')
 	longitude=$(curl -s https://json.geoiplookup.io/"$(curl -s https://ipinfo.io/ip)" | jq '.longitude')
-	echo "$latitude"
-	echo "$longitude"
+	unit=imperial
 	deleteFile weatherData.json
-	curl -s https://api.openweathermap.org/data/2.5/onecall?lat="$latitude"\&lon="$longitude"\&exclude=minutely,hourly\&appid=4a7d63f695a6c50153cba0e4d5b5c27e | jq  > weatherData.json
+	curl -s https://api.openweathermap.org/data/2.5/onecall?lat="$latitude"\&lon="$longitude"\&units="$unit"\&exclude=minutely,hourly\&appid=4a7d63f695a6c50153cba0e4d5b5c27e | jq  > weatherData.json
     chmod 600 weatherData.json
 }
 
 getWeatherData 
 bash createTable.sh
 python3 main.py
+#DROP TABLE visit_infomation, weather_current, weather_daily;
 
 
